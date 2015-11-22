d449d54f21a74b6bdf1c2863667c30c1d6f14233
Kevin Grigorenko <kevin@myplaceonline.com>
2015-11-22 12:36:36 -0800

    Debug

--- www/js/myplaceonline.js
+++ www/js/myplaceonline.js
@@ -770,12 +770,9 @@ function ensureClipboard(objects) {
   // If we're in PhoneGap, use the clipboard plugin; otherwise,
   // check if the user has overriden clipboard integration and use that
   // option or fall back to ZeroClipboard
-  alert(cordova && cordova.plugins && cordova.plugins.clipboard);
-  if (window.plugins && window.plugins.clipboard) {
+  if (cordova && cordova.plugins && cordova.plugins.clipboard) {
     $("[data-clipboard-text]").click( function(e) {
-      alert('here1');
-      window.plugins.clipboard.copy($(this).data("clipboard-text"));
-      alert('here2');
+      cordova.plugins.clipboard.copy($(this).data("clipboard-text"));
       createSuccessNotification("Copied '" + $(this).data("clipboard-text") + "' to clipboard (c1).");
       //e.preventDefault();
       return true;
